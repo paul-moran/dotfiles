7b2e8d9f4cbce59a4077b164823e0c1882971150
Greg MacWilliam <gmacwill77@gmail.com>
2021-12-14 10:42:09 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+alias copme='dev style --include-branch-commits'
+alias typeme='bin/srb typecheck'
+alias dumpme='dev dump-graphql admin'
+alias packme='dev packages check'
+alias recast_all='dev rbi dsl'
+
+recast() {
+  dev rbi dsl $1
+}
