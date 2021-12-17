989e67477502a77b202b9034a44d719e0a04491f
Greg MacWilliam <gmacwill77@gmail.com>
2021-12-17 08:41:23 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+alias spinme='systemctl restart gcs-secrets.service && systemctl restart template-config.service'
 alias copme='dev style --include-branch-commits'
 alias typeme='bin/srb typecheck'
 alias dumpme='dev dump-graphql admin'
