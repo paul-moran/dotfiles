b33380191f5164203a098e46d63418ee28370d5f
Greg MacWilliam <gmacwill77@gmail.com>
2022-01-10 10:18:18 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+install_links() {
+    dir=~/dotfiles             # dotfiles directory
+    files="zshrc gitconfig"    # list of files/folders to symlink in homedir
+
+    for file in $files; do
+      echo "Creating symlink to .$file"
+      ln -sf $dir/.$file ~/.$file
+    done
+}
+
+install_links
